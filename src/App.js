1ea5c022f447ab00c0590995dadc2e6736844ad5
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-02 15:13:13 +0100

    Add React Router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { Container, Alert, Dropdown } from "react-bootstrap";
 import MyNavbar from "./components/MyNavbar";
 import MyFooter from "./components/MyFooter";
 import MovieList from "./components/MovieList";
-import { browseRouter, Routers, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Tvshows from "./components/TvShows";
 
 const App = () => {
   const [gallery1, setGallery1] = useState([]);
@@ -60,59 +61,64 @@ const App = () => {
 
   return (
     <div>
-      <MyNavbar />
-      <Container fluid className="px-4">
-        <div className="d-flex justify-content-between">
-          <div className="d-flex">
-            <h2 className="mb-4">TV Shows</h2>
-            <div className="ms-4 mt-1">
-              <Dropdown>
-                <Dropdown.Toggle
-                  style={{ backgroundColor: "#221f1f" }}
-                  id="dropdownMenuButton"
-                  className="btn-secondary btn-sm dropdown-toggle rounded-0"
-                >
-                  Genres
-                </Dropdown.Toggle>
-                <Dropdown.Menu bg="dark">
-                  <Dropdown.Item href="#/action-1">Comedy</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Drama</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">Thriller</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+      <BrowserRouter>
+        <MyNavbar />
+        <Container fluid className="px-4">
+          <div className="d-flex justify-content-between">
+            <div className="d-flex">
+              <h2 className="mb-4">TV Shows</h2>
+              <div className="ms-4 mt-1">
+                <Dropdown>
+                  <Dropdown.Toggle
+                    style={{ backgroundColor: "#221f1f" }}
+                    id="dropdownMenuButton"
+                    className="btn-secondary btn-sm dropdown-toggle rounded-0"
+                  >
+                    Genres
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu bg="dark">
+                    <Dropdown.Item href="#/action-1">Comedy</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Drama</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Thriller</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            </div>
+            <div>
+              <i className="fa fa-th-large icons"></i>
+              <i className="fa fa-th icons"></i>
             </div>
           </div>
-          <div>
-            <i className="fa fa-th-large icons"></i>
-            <i className="fa fa-th icons"></i>
-          </div>
-        </div>
-        {error && (
-          <Alert variant="danger" className="text-center">
-            An error has occurred, please try again!
-          </Alert>
-        )}
-        {!error && (
-          <>
-            <MovieList
-              title="Harry Potter"
-              loading={loading}
-              movies={gallery1.slice(0, 6)}
-            />
-            <MovieList
-              title="Lord of the Rings"
-              loading={loading}
-              movies={gallery2.slice(0, 6)}
-            />
-            <MovieList
-              title="Star Wars"
-              loading={loading}
-              movies={gallery3.slice(0, 6)}
-            />
-          </>
-        )}
-        <MyFooter />
-      </Container>
+          {error && (
+            <Alert variant="danger" className="text-center">
+              An error has occurred, please try again!
+            </Alert>
+          )}
+          {!error && (
+            <>
+              <MovieList
+                title="Harry Potter"
+                loading={loading}
+                movies={gallery1.slice(0, 6)}
+              />
+              <MovieList
+                title="Lord of the Rings"
+                loading={loading}
+                movies={gallery2.slice(0, 6)}
+              />
+              <MovieList
+                title="Star Wars"
+                loading={loading}
+                movies={gallery3.slice(0, 6)}
+              />
+            </>
+          )}
+          <MyFooter />
+        </Container>
+        <Routes>
+          <Route path="/" element={<Tvshows />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
